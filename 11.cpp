8939d7973dc4c0bb59fb6914a82646bd692abb57
Nguyen Ngoc Quyen <84521614+quyen99sky@users.noreply.github.com>
2022-05-24 17:21:16 +0700

    Add files via upload

---
+++ 11.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+int main(){
+	int var;
+	printf("Nhap var = ");
+	scanf("%d", &var);
+	printf("\n Gia tri cua var = %d", var);
+	printf("\n Dia chi cua bien var trong bo nho la: %p", &var);
+	
+	
+
+	return 0;
+}
+
